ec2c5dfca4e993d4e80a739003e005e0c5bb5e9e
pamelamoreiras <pamelamoreiras99@gmail.com>
2023-06-28 11:57:39 -0300

    Adicionando funcionalidades no to do list

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+//Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -7,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class TodoListComponent {
 
+  public taskList: Array<TaskList> = [
+    { task: "Minha nova task", checked: true},
+    { task: "Minha nova task2", checked: false}
+  ];
 }
