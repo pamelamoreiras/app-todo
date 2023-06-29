e146236cfc434c55f66572cb59145c7d31dac0e4
pamelamoreiras <pamelamoreiras99@gmail.com>
2023-06-29 10:20:30 -0300

    Adiciona a funcionalidade local storage

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -11,12 +11,10 @@ import { TaskList } from '../../model/task-list';
 export class TodoListComponent implements DoCheck {
   
 
-  public taskList: Array<TaskList> = [
-    
-  ];
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
 
   ngDoCheck(): void {
-    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+    this.setLocalStorage();
   }
 
   public setEmitTaskList(event: string) {
@@ -43,6 +41,13 @@ export class TodoListComponent implements DoCheck {
       if(confirm) {
         this.deleteItemTaskList(index);
       }
+    }
+  }
+
+  public setLocalStorage() {
+    if(this.taskList) {
+      this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+      localStorage.setItem("list", JSON.stringify(this.taskList));
     }
   }
 }
