75f5c04b19185082c47b125c5d502aea81644fdb
pamelamoreiras <pamelamoreiras99@gmail.com>
2023-06-29 09:47:53 -0300

    Implementa a adição de itens na lista

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -11,17 +11,21 @@ import { TaskList } from '../../model/task-list';
 export class TodoListComponent {
 
   public taskList: Array<TaskList> = [
-    { task: "Minha nova task", checked: true},
-    { task: "Minha nova task2", checked: false}
+    
   ];
+
+  public setEmitTaskList(event: string) {
+    this.taskList.push({task: event, checked: false})
+    
+  }
 
   public deleteItemTaskList(event: number) {
     this.taskList.splice(event,1);
   }
 
   public deleteAllTaskList() {
-    const confirm = window.confirm("Você deseja realmente DELETAR tudo?")
-    
+    const confirm = window.confirm("Você deseja DELETAR tudo?")
+
     if(confirm){
       this.taskList = [];
     }
